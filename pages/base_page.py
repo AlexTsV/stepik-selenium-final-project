06a2619aee864133f7c8794d71ed2ddde3289277
AlexTsV <alex944480@gmail.com>
2019-09-10 15:20:43 +0300

    test login url

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,7 +2,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage():
-    def __init__(self, browser, url, timeout=30):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -16,3 +16,10 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def is_login_url(self):
+        login_url = self.browser.current_url('http://selenium1py.pythonanywhere.com/')
+        if 'login' in login_url:
+            return True
+        else:
+            return False
