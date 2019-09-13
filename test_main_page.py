a0b7607bc88cdd2b66d601e3b4f8f4c10f4723ff
AlexTsV <alex944480@gmail.com>
2019-09-13 11:38:42 +0300

    debug test_main_page.py test_product_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,7 @@ from pages.login_page import LoginPage
 import pytest
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -14,6 +14,7 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
 
 
+# @pytest.mark.skip
 def test_guest_should_see_login_button(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -24,6 +25,7 @@ def test_guest_should_see_login_button(browser):
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = 'http://selenium1py.pythonanywhere.com'
     page = BasketPage(browser, link)
+    page.open()
     page.go_to_basket_from_navbar()
     page.should_be_no_items_in_basket()
     page.should_be_message_basket_is_empty()
